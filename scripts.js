a35fc1cef49e4fe19a98786684b81a3c4bdc8ed2
Brandon Haak <bhaak2@uic.edu>
2019-01-03 22:56:03 -0600

    Final Commit for 1/3/2019

---
+++ scripts.js
@@ -0,0 +1,14 @@
+//Function to toggle main menu
+function menuToggle(x) {
+  x.classList.toggle("change");
+}
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "400px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
