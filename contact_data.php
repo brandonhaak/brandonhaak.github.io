b1862255530c621733e787b65741ef019379d7fe
Brandon Haak <bhaak2@uic.edu>
2019-01-18 12:52:08 -0600

    Discovered that github does not support PHP server-side code...

--- contact_data.php
+++ contact_data.php
@@ -1,3 +1,10 @@
-Welcome <?php echo $_POST['firstname']; ?> <?php echo $_POST['lastname']; ?><br>
-Your email address is: <?php echo $_POST['email']; ?><br>
-And you want to say: <?php echo $_POST['message']; ?>
+<!DOCTYPE html>
+<html>
+<body>
+
+Welcome <?php echo $_POST["firstname"]; ?> <?php echo $_POST["lastname"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+And you want to say: <?php echo $_POST["message"]; ?>
+
+</body>
+</html>
